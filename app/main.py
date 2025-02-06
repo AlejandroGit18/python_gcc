1813573e0c5fa8d9f55bf0c8c0288f9a17105aec
Alejandro Perez <perezalejandrodl14@gmail.com>
2025-02-06 06:38:13 -0600

    commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum  # Importar Mangum para Vercel
 from app.routers import (
     auth,
     consulta_saldo_cuenta,
@@ -34,19 +35,19 @@ app = FastAPI()
 
 # Configuración de CORS
 origins = [
-    "http://localhost",        # Para pruebas locales
-    "http://localhost:3000",   # Para un frontend local
-    "http://127.0.0.1:5500",   # Otra dirección local
-    "http://127.0.0.1:3000",   # Otra dirección local
-    "https://GCC.com",  # Dominios específicos permitidos
+    "http://localhost",        
+    "http://localhost:3000",   
+    "http://127.0.0.1:5500",   
+    "http://127.0.0.1:3000",   
+    "https://GCC.com",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Permitir estos orígenes
-    allow_credentials=True,  # Permitir cookies/credenciales
-    allow_methods=["*"],     # Permitir todos los métodos HTTP
-    allow_headers=["*"],     # Permitir todos los encabezados
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"],     
+    allow_headers=["*"],     
 )
 
 # Registrar rutas existentes
@@ -56,9 +57,7 @@ app.include_router(roles.router, prefix="/api", tags=["roles"])
 app.include_router(campanas.router, prefix="/api", tags=["campanas"])
 app.include_router(campanas_usuarios.router, prefix="/api", tags=["campanas_usuarios"])
 app.include_router(gestiones.router, prefix="/api", tags=["gestiones"])
-app.include_router(encryption.router, prefix="/api", tags=["encriptar"])  # Registrar rutas de encriptación
-
-# Registrar nuevas rutas
+app.include_router(encryption.router, prefix="/api", tags=["encriptar"])
 app.include_router(modulos.router, prefix="/api", tags=["modulos"])
 app.include_router(modulos_usuarios.router, prefix="/api", tags=["modulos_usuarios"])
 app.include_router(drop.router, prefix="/api", tags=["drops"])
@@ -69,7 +68,6 @@ app.include_router(tipo_descuento.router, prefix="/api", tags=["tipo_descuento"]
 app.include_router(carga_tipologias.router, prefix="/api", tags=["carga_tipologias"])
 app.include_router(consulta_tipologias.router, prefix="/api", tags=["consulta_tipologias"])
 app.include_router(registrar_pago.router, prefix="/api", tags=["registrar_pago"])
-
 app.include_router(reporte_pagos.router, prefix="/api", tags=["reporte_pagos"])
 app.include_router(reporte_promesas.router, prefix="/api", tags=["reporte_promesas"])
 app.include_router(consulta_saldo_cuenta.router, prefix="/api", tags=["consulta_saldo_cuenta"])
@@ -80,7 +78,9 @@ app.include_router(reporte_tipologias.router, prefix="/api", tags=["reporte_tipo
 app.include_router(consulta_usuarios_carga_gestiones.router, prefix="/api", tags=["consulta_usuarios_carga_gestiones"])
 app.include_router(consulta_historico_pagos.router, prefix="/api", tags=["consulta_historico_pagos"])
 
-
 @app.get("/")
 def root():
     return {"message": "Welcome to FastAPI"}
+
+# Adaptador de FastAPI para Vercel
+handler = Mangum(app)
